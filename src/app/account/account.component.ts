b1b54b46fc8a46065e20d93353e1390e86745d04
Dragos Boboluta <dragos.boboluta@nagarro.com>
2022-09-15 22:12:48 +0300

    formating and some minor changes

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AccountModel, CreateAccountModel } from './account.model';
 import { AccountService } from './service/account.service';
-import { AccountId, MockAccount } from './account.mock';
 import { TransactionService } from '../transaction/service/transaction.service';
 import { TransactionModel } from '../transaction/transaction.model';
 import { UserService } from '../user/service/user.service';
@@ -13,17 +12,15 @@ import { UserService } from '../user/service/user.service';
 export class AccountComponent implements OnInit {
   accounts: AccountModel[] = [];
   accountTransactions : TransactionModel[]=[];
-  
+  public name: string = this.userService.currentUser.username;
 
-  constructor(private userService : UserService,public accountService: AccountService, public transactionService: TransactionService) { }
+  constructor(public userService : UserService,public accountService: AccountService, public transactionService: TransactionService) { }
 
   ngOnInit(): void {
     this.accountService.getAccounts(this.userService.currentUser.userId).subscribe(response => {
       this.accounts = response;
-
+      
     });
-    
-
     // this.accountService.createAccounts(MockAccount).subscribe();
     // this.accountService.deleteAccount(AccountId).subscribe();
     // this.accountService.updateAccount(AccountId).subscribe();
