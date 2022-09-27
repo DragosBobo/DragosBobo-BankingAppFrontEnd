153b9905b427ffa9289bf5d6d2e8f82d0a05061e
Dragos Boboluta <dragos.boboluta@nagarro.com>
2022-09-27 15:51:12 +0300

    filter works for tabel

--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { ActivatedRoute } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
 import { SubSink } from 'subsink';
@@ -18,6 +19,7 @@ export class TransactionComponent implements OnInit,OnDestroy {
   id: any;
   notifier = new Subject();
   transactionSlice : TransactionModel[] = [];
+  dataSource = new MatTableDataSource(this.transactionSlice);
   displayedColumns = ['num','categoryName','totalAmount'];
   constructor(private transactionService: TransactionService, private activatedRoute: ActivatedRoute,private matRef: MatDialog) {
 
@@ -29,7 +31,7 @@ export class TransactionComponent implements OnInit,OnDestroy {
   ngOnInit(): void {
     this.id = this.activatedRoute.snapshot.paramMap.get("id");
     if (this.id)
-     this.transactionService.fetchTransactions().pipe(takeUntil(this.notifier)).subscribe(response => {this.transactions=response;this.transactionSlice=this.transactions.slice(0,20)});
+     this.transactionService.fetchTransactions().pipe(takeUntil(this.notifier)).subscribe(response => {this.transactions=response;this.transactionSlice=this.transactions.slice(0,20); this.dataSource  = new MatTableDataSource(this.transactionSlice);});
   }
   onPageChange(event: PageEvent) {
     const startIndex = event.pageIndex * event.pageSize;
@@ -40,5 +42,13 @@ export class TransactionComponent implements OnInit,OnDestroy {
   generateRaport(id:string){
     console.log(`generate raport for account wiht id : ${id}`);
     this.matRef.open(RaportComponent).afterClosed().pipe(takeUntil(this.notifier)).subscribe(result=>console.log("modal works"))
+  }
+  applyFilter(event: Event) {
+    this.dataSource  = new MatTableDataSource(this.transactionSlice);
+    let filterValue = (event.target as HTMLInputElement).value;
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.dataSource.filter = filterValue;
+
   }
 }
