1beb07c3f4117c37bcd47149d02712671428a260
Dragos Boboluta <dragos.boboluta@nagarro.com>
2022-09-28 13:02:47 +0300

    samll fixes

--- src/app/account/modal/delete/delete-modal/delete-modal.component.ts
+++ src/app/account/modal/delete/delete-modal/delete-modal.component.ts
@@ -6,24 +6,30 @@ import { AccountService } from 'src/app/account/service/account.service';
 @Component({
   selector: 'app-delete-modal',
   templateUrl: './delete-modal.component.html',
-  styleUrls: ['./delete-modal.component.scss']
+  styleUrls: ['./delete-modal.component.scss'],
 })
 export class DeleteModalComponent implements OnInit, OnDestroy {
   notifier = new Subject();
-  accId !: string;
-  constructor(private account: AccountService, private ref: MatDialogRef<DeleteModalComponent>, @Inject(MAT_DIALOG_DATA) public data: string) { this.accId = data }
+  accId!: string;
+  constructor(
+    private account: AccountService,
+    private ref: MatDialogRef<DeleteModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: string
+  ) {
+    this.accId = data;
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   ngOnDestroy(): void {
     this.notifier.complete();
   }
 
-  ngOnInit(): void {
-  }
-
   agree() {
-    this.account.deleteAccount(this.accId).pipe(
-      takeUntil(this.notifier)
-
-    ).subscribe(response => this.ref.close(response));
+    this.account
+      .deleteAccount(this.accId)
+      .pipe(takeUntil(this.notifier))
+      .subscribe(response => this.ref.close(response));
   }
   disagree() {
     this.ref.close();
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './service/user.service';
 
-
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
 })
 export class UserComponent implements OnInit {
-
-  constructor(public userService:UserService) { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(public userService: UserService) {}
 }
