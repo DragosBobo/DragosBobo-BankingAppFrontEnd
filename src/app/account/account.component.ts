9320b2a575ab94ce5bb1de54cb3800a498dc2a94
Dragos Boboluta <dragos.boboluta@nagarro.com>
2022-09-28 11:33:10 +0300

    subsink delete

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnDestroy, OnInit, Pipe, PipeTransform } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+  Pipe,
+  PipeTransform,
+} from '@angular/core';
 import { AccountModel, CreateAccountModel } from './account.model';
 import { AccountService } from './service/account.service';
 import { TransactionService } from '../transaction/service/transaction.service';
@@ -10,49 +17,69 @@ import { MatDialog } from '@angular/material/dialog';
 import { ModalComponent } from './modal/modal/modal.component';
 import { modal } from './account.model';
 import { DeleteModalComponent } from './modal/delete/delete-modal/delete-modal.component';
-import { SubSink } from 'subsink';
 import { Subject, takeUntil } from 'rxjs';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  styleUrls: ['./account.component.scss'],
 })
 export class AccountComponent implements OnInit, OnDestroy {
   accounts: AccountModel[] = [];
   accountTransactions: TransactionModel[] = [];
   name: string = this.userService.currentUser.username;
-  id = localStorage.getItem("id");
+  id = localStorage.getItem('id');
   notifier = new Subject();
   accountSlice: AccountModel[] = [];
-  constructor(private userService: UserService, private matRef: MatDialog, private router: Router, private accountService: AccountService, private transactionService: TransactionService) { }
+  constructor(
+    private userService: UserService,
+    private matRef: MatDialog,
+    private router: Router,
+    private accountService: AccountService,
+    private transactionService: TransactionService
+  ) {}
   ngOnDestroy(): void {
     this.notifier.complete();
   }
 
   ngOnInit(): void {
     if (this.id != null) {
-     this.accountService.getAccounts(this.id).pipe(takeUntil(this.notifier)).subscribe(accounts => {
-        this.accounts = accounts;
-        this.accounts.unshift(modal);
-        this.accountSlice = this.accounts.slice(0, 4);
-      })
+      this.accountService
+        .getAccounts(this.id)
+        .pipe(takeUntil(this.notifier))
+        .subscribe((accounts) => {
+          this.accounts = accounts;
+          this.accounts.unshift(modal);
+          this.accountSlice = this.accounts.slice(0, 4);
+        });
     }
   }
   onPageChange(event: PageEvent) {
     const startIndex = event.pageIndex * event.pageSize;
     let endIndex = startIndex + event.pageSize;
-    if (endIndex > this.accounts.length) { endIndex = this.accounts.length; }
+    if (endIndex > this.accounts.length) {
+      endIndex = this.accounts.length;
+    }
     this.accountSlice = this.accounts.slice(startIndex, endIndex);
   }
   onClickModal() {
-   this.matRef.open(ModalComponent).afterClosed().pipe(takeUntil(this.notifier)).subscribe(val => { if (val == "Account created with succes ! ") { this.ngOnInit() } })
+    this.matRef
+      .open(ModalComponent)
+      .afterClosed()
+      .pipe(takeUntil(this.notifier))
+      .subscribe((val) => {
+        if (val == 'Account created with succes ! ') {
+          this.ngOnInit();
+        }
+      });
   }
   logout() {
     localStorage.clear();
   }
   deleteAccount(id: string) {
-    this.matRef.open(DeleteModalComponent, { data: id }).afterClosed().pipe(takeUntil(this.notifier)).subscribe(response => this.ngOnInit())
+    this.matRef
+      .open(DeleteModalComponent, { data: id })
+      .afterClosed()
+      .pipe(takeUntil(this.notifier))
+      .subscribe((response) => this.ngOnInit());
   }
 }
-
-
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -2,30 +2,39 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
-import { SubSink } from 'subsink';
 import { UserService } from '../service/user.service';
 import { IUserModel } from '../user.model';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  loginForm !: FormGroup;
-  token !: string;
+  loginForm!: FormGroup;
+  token!: string;
   notifier = new Subject();
-  constructor(private formBuilder: FormBuilder, private userService: UserService, private router: Router) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {}
   ngOnDestroy(): void {
     this.notifier.complete();
   }
 
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      email: [""],
-      password: [""]
-    })
+      email: [''],
+      password: [''],
+    });
   }
   login() {
-    this.userService.loginUser(this.loginForm).pipe(takeUntil(this.notifier)).subscribe(res => { console.log(res); this.router.navigate(["/account"]).then(() => window.location.reload()) });
+    this.userService
+      .loginUser(this.loginForm)
+      .pipe(takeUntil(this.notifier))
+      .subscribe((res) => {
+        console.log(res);
+        this.router.navigate(['/account']).then(() => window.location.reload());
+      });
   }
 }
