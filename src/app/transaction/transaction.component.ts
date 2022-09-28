e2e938245aed9ff6d2cc58958766ec547aa52e92
Dragos Boboluta <dragos.boboluta@nagarro.com>
2022-09-28 12:33:00 +0300

    delete subsink

--- src/app/transaction/transaction.component.ts
+++ src/app/transaction/transaction.component.ts
@@ -1,29 +1,29 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
-import { SubSink } from 'subsink';
 import { TransactionService } from './service/transaction.service';
 import { TransactionId, CreateTransaction, RaportTransaction } from './transaction.mock';
 import { TransactionModel } from './transaction.model';
 @Component({
   selector: 'app-transaction',
   templateUrl: './transaction.component.html',
-  styleUrls: ['./transaction.component.scss']
+  styleUrls: ['./transaction.component.scss'],
 })
-export class TransactionComponent implements OnInit,OnDestroy {
+export class TransactionComponent implements OnInit, OnDestroy {
   transactions: TransactionModel[] = [];
   id: any;
   notifier = new Subject();
-  constructor(private transactionService: TransactionService, private activatedRoute: ActivatedRoute) {
-
-  }
+  constructor(private transactionService: TransactionService, private activatedRoute: ActivatedRoute) {}
   ngOnDestroy(): void {
     this.notifier.complete();
   }
 
   ngOnInit(): void {
-    this.id = this.activatedRoute.snapshot.paramMap.get("id");
+    this.id = this.activatedRoute.snapshot.paramMap.get('id');
     if (this.id)
-     this.transactionService.fetchTransactionsByAccountId(this.id).pipe(takeUntil(this.notifier)).subscribe(response => this.transactions = response);
+      this.transactionService
+        .fetchTransactionsByAccountId(this.id)
+        .pipe(takeUntil(this.notifier))
+        .subscribe((response: TransactionModel[]) => (this.transactions = response));
   }
 }
