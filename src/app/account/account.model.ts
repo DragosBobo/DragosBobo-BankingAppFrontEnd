540eaefc7d1adaf6b1444fc4eaf5912d204dc2f2
Dragos Boboluta <dragos.boboluta@nagarro.com>
2022-09-29 11:04:43 +0300

    use ngFor  in  modal template

--- src/app/account/account.model.ts
+++ src/app/account/account.model.ts
@@ -1,30 +1,29 @@
+interface AccountModel {
+  currency: string;
+  accountType: string;
+  iban: string;
+  accountId: string;
+}
+interface CreateAccountModel {
+  accountType: AccountType;
+  currency: Currency;
+  iban: string;
+  userId: string;
+}
+const modal: AccountModel = {
+  accountId: 'modal',
+  accountType: 'modal',
+  currency: 'modal',
+  iban: 'modal',
+};
+enum AccountType {
+  Debit,
+  Credit,
+}
+enum Currency {
+  Ron,
+  Euro,
+  Dollar,
+}
 
-interface AccountModel{
-    currency:string;
-    accountType:string;
-    iban:string;
-    accountId:string;
-}
- interface CreateAccountModel{
-    accountType:AccountType;
-    currency:Currency;
-    iban:string;
-    userId:string;
-}
-const modal : AccountModel = {
-    "accountId"
-      :
-      "modal",
-    "accountType"
-      :
-      "modal",
-    "currency"
-      :
-      "modal",
-    "iban"
-      :
-      "modal"
-  };
-enum AccountType { Debit = 0, Credit = 1}
-enum Currency { Ron = 0, Euro =1, Dollar=2}
-export { AccountModel, CreateAccountModel ,AccountType,Currency,modal}
+export { AccountModel, CreateAccountModel, AccountType, Currency, modal };
--- src/app/account/modal/add-account-modal/add-account-modal.component.ts
+++ src/app/account/modal/add-account-modal/add-account-modal.component.ts
@@ -5,6 +5,7 @@ import { AccountType, CreateAccountModel, Currency } from '../../account.model';
 import { AccountService } from '../../service/account.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms';
+import { Type } from '@angular/compiler';
 @Component({
   selector: 'app-add-account-modal',
   templateUrl: './add-account-modal.component.html',
@@ -12,13 +13,16 @@ import { FormBuilder, FormControl, FormGroup, NgForm } from '@angular/forms';
 })
 export class AddAccountModalComponent implements OnDestroy {
   accountTypeValues: AccountType[] = [0, 1];
-  currencyValues: Currency[] = [0, 1, 2];
-  x: Currency = 0;
+  currencyCategories: Currency[] = [0, 1, 2];
+  public currencyName = Object.keys(Currency).filter(v => isNaN(Number(v))) as (keyof typeof Currency)[];
+  public typeName = Object.keys(AccountType).filter(v => isNaN(Number(v))) as (keyof typeof AccountType)[];
   notifier = new Subject();
+
   id = localStorage.getItem('id');
   constructor(private accountService: AccountService, private ref: MatDialogRef<AddAccountModalComponent>) {}
   ngOnDestroy(): void {
     this.notifier.complete();
+    console.log(this.currencyName);
   }
 
   addAccount(acc: CreateAccountModel) {
